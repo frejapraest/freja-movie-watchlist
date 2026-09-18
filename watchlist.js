27590e3e28418b55d2b6fe226d301a46cc31bc35
frejapraest <frejapraest@gmail.com>
2026-09-18 21:09:18 +0200

    rendered movies to localStorage and added remove button

--- watchlist.js
+++ watchlist.js
@@ -2,5 +2,16 @@ import { generateMovieCard } from "./helpers.js";
 const watchlistEl = document.getElementById("watchlist");
 
 for (const movie of Object.values({ ...localStorage })) {
-  watchlistEl.innerHTML += generateMovieCard(JSON.parse(movie), true);
+  const movieObj = JSON.parse(movie);
+  watchlistEl.insertAdjacentHTML(
+    "beforeend",
+    generateMovieCard(movieObj, true),
+  );
+
+  document
+    .getElementById(`button-${movieObj.imdbID}`)
+    .addEventListener("click", () => {
+      localStorage.removeItem(movieObj.imdbID);
+      document.getElementById(movieObj.imdbID).remove();
+    });
 }
