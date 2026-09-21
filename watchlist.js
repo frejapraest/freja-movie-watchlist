3ba1739e32b0e393c9f34ae1dc6c97b50b5f2b50
frejapraest <frejapraest@gmail.com>
2026-09-21 20:51:39 +0200

    Remove watchlist placeholder when the watchlist contains movies

--- watchlist.js
+++ watchlist.js
@@ -1,6 +1,17 @@
 import { generateMovieCard } from "./helpers.js";
 
 const watchlistEl = document.getElementById("watchlist");
+const placeholderContainerWatchlistEl = document.getElementById(
+  "placeholder-container-watchlist",
+);
+
+const checkList = (array) => {
+  array.length <= 0
+    ? placeholderContainerWatchlistEl.classList.remove("hidden")
+    : placeholderContainerWatchlistEl.classList.add("hidden");
+};
+
+checkList(localStorage);
 
 for (const movie of Object.values(localStorage)) {
   const movieObj = JSON.parse(movie);
@@ -8,10 +19,12 @@ for (const movie of Object.values(localStorage)) {
     "beforeend",
     generateMovieCard(movieObj, true),
   );
+
   document
     .getElementById(`button-${movieObj.imdbID}`)
     .addEventListener("click", () => {
       localStorage.removeItem(movieObj.imdbID);
       document.getElementById(movieObj.imdbID).remove();
+      checkList(localStorage);
     });
 }
